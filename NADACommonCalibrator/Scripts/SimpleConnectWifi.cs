90a6301ff8e652abbea4f19ffaafd9807d13ac4d
lsw <lsw8724@nada.co.kr>
2016-10-23 18:15:49 +0900

    -

---
+++ NADACommonCalibrator/Scripts/SimpleConnectWifi.cs
@@ -0,0 +1,32 @@
+﻿using NADACommonCalibrator;
+using NADACommonCalibrator.Receiver;
+using NCCCommon;
+using NCCCommon.ModuleProtocol;
+using NCCCommon.ModuleProtocol.OmapProtocol;
+using NADACommonCalibrator.PlotControl;
+
+public class Items
+{
+    public float? Ch1 { get; set; }
+    public float? Rpm { get; set; }
+}
+
+public class NCCScript
+{
+    public string Description { get { return "Wfs Connect Test"; } }
+    public int AsyncFMax { get; set; }
+    public int AsyncLine { get; set; }
+    public ReceiverWifi Receiver = new ReceiverWifi();
+
+    public NCCScript()
+    {
+        AsyncFMax = 3200;
+        AsyncLine = 3200;
+    }
+
+    public void Run()
+    {
+        Receiver.Start();
+    }
+}
+
---
+++ NADACommonCalibrator/bin/Release/Scripts/SimpleConnectVirtualModule.cs
@@ -0,0 +1,39 @@
+﻿using NADACommonCalibrator;
+using NADACommonCalibrator.Receiver;
+using NCCCommon;
+using NCCCommon.ModuleProtocol;
+using NCCCommon.ModuleProtocol.OmapProtocol;
+using NADACommonCalibrator.PlotControl;
+
+public class Items
+{
+    public float? Ch1 { get; set; }
+    public float? Ch2 { get; set; }
+    public float? Ch3 { get; set; }
+    public float? Ch4 { get; set; }
+    public float? Ch5 { get; set; }
+    public float? Ch6 { get; set; }
+    public float? Ch7 { get; set; }
+    public float? Ch8 { get; set; }
+    public float? Rpm { get; set; }
+}
+
+public class NCCScript
+{
+    public string Description { get { return "Virtual Connect Test"; } }
+    public int AsyncFMax { get; set; }
+    public int AsyncLine { get; set; }
+    public ReceiverVirtual Receiver = new ReceiverVirtual();
+    
+    public NCCScript()
+    {
+        AsyncFMax = 3200;
+        AsyncLine = 800;
+    }
+
+    public void Run()
+    {
+        Receiver.Start();
+    }
+}
+
